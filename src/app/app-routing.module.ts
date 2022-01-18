529af1dcd322b266084cca2ef1aaf8fd8f850301
jawbreaker666 <andrasev96@web.de>
2022-01-18 09:41:25 +0100

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataProtectionComponent } from './data-protection/data-protection.component';
+import { HeaderComponent } from './header/header.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 
 const routes: Routes = [
 
-  
+  { path: '', component: HeaderComponent},
   { path: 'impressum', component: ImpressumComponent},
   { path: 'data-protection', component: DataProtectionComponent},
 ];
