7f4d7ad41fbbe60031046648d5a608c6f5660f1e
jawbreaker666 <andrasev96@web.de>
2022-01-24 09:22:13 +0100

    24.1 responsive

---
+++ src/assets/kochwelt/Aktuell-Kochwelt_22.11.2021/Gruppenarbeit Kochwelt Laura/pastapesto.js
@@ -0,0 +1,112 @@
+function calculate() {
+    let howManyPortions = document.getElementById('input');
+    let nudeln = document.getElementById('aNudeln');
+
+    let nudelnResult = howManyPortions.value * 1;
+
+    
+    
+    nudeln.innerHTML = `Nudeln: ${nudelnResult} g (ca. ${nudelnResult*125}g)`;
+
+    start();
+
+    howManyPortions.value = '';
+
+}
+
+function start() {
+    
+    calculatePesto();
+    calculateMilk();
+    calculatesaureSahne();
+    calculateParmesan();
+    calculateRucola();
+    calculatePinienkerne();
+    calculateKnoblauch();
+    calculateCrèmefraîche();
+    calculateOlivenöl();
+}
+
+function calculatePesto() {
+    let howManyPortions = document.getElementById('input').value;
+    let pesto = document.getElementById('pesto');
+
+    let pestoResult = howManyPortions * 1;
+
+    pesto.innerHTML = `Pesto: ${pestoResult} bis ${pestoResult*2} TL`;
+}
+
+function calculateMilk() {
+    let howManyPortions = document.getElementById('input').value;
+    let milk = document.getElementById('milk');
+
+    let milkResult = howManyPortions * 0.5
+
+    milk.innerHTML = `Milch: ${milkResult} ml`;
+
+}
+
+function calculatesaureSahne() {
+    let howManyPortions = document.getElementById('input').value;
+    let sauresahne = document.getElementById('saureSahne');
+
+    let sauresahneResult = howManyPortions * 3;
+
+    sauresahne.innerHTML = `saure Sahne: ${sauresahneResult} g`;
+}
+
+function calculateParmesan() {
+    let howManyPortions = document.getElementById('input').value;
+    let parmesan = document.getElementById('parmesan');
+
+    let parmesanResult = howManyPortions * 2;
+
+    parmesan.innerHTML = `Parmesan: ${parmesanResult} g`;
+}
+
+function calculateRucola() {
+    let howManyPortions = document.getElementById('input').value;
+    let rucola = document.getElementById('rucola');
+
+    let rucolaResult = howManyPortions * 0.5;
+
+    rucola.innerHTML = `Rucola: ${rucolaResult} g`;
+}
+
+function calculatePinienkerne() {
+    let howManyPortions = document.getElementById('input').value;
+    let pinienkerne = document.getElementById('pinienkerne');
+
+    let pinienkerneResult = howManyPortions * 1;
+
+    pinienkerne.innerHTML = `Pinienkerne oder Sonnenblumenkerne: ${pinienkerneResult} g`;
+}
+
+function calculateKnoblauch() {
+    let howManyPortions = document.getElementById('input').value;
+    let knoblauch = document.getElementById('knoblauch');
+
+    let knoblauchResult = howManyPortions * 1;
+
+    knoblauch.innerHTML = `Knoblauch: ${kuemmelResult} Zehe`;
+}
+
+function calculateCrèmefraîche() {
+    let howManyPortions = document.getElementById('input').value;
+    let crèmefraîche = document.getElementById('crèmefraîche');
+
+    let crèmefraîcheResult = howManyPortions * 1;
+
+    crèmefraîche.innerHTML = `Crèmefraîche: ${kuemmelResult} EL`;
+}
+
+function calculateOlivenöl() {
+    let howManyPortions = document.getElementById('input').value;
+    let olivenöl = document.getElementById('olivenöl');
+
+    let olivenölResult = howManyPortions * 1;
+
+    olivenöl.innerHTML = `Olivenöl: ${kuemmelResult} EL`;
+}
+
+
