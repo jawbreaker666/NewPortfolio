7fe8eade120411738146795f7d60f0a41a06c22e
jawbreaker666 <andrasev96@web.de>
2022-01-25 10:07:05 +0100

    finish

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'MeinPortfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MeinPortfolio');
+    expect(app.laura-andrasev-portfolio).toEqual('MeinPortfolio');
   });
 
   it('should render title', () => {
